ad94edfe30147f0c763862a1dada69363c2194e0
Geonhyeong <42195466+Geonhyeong@users.noreply.github.com>
2021-11-24 13:01:55 +0900

    upload image function update

--- yolov5/image.py
+++ yolov5/image.py
@@ -1,24 +1,61 @@
 # pip install firebase_admin
 import firebase_admin
-from firebase_admin import credentials, db
+from firebase_admin import credentials, db, storage
 
 from PIL import Image
+import io
 
 cred = credentials.Certificate('./ServiceAccountKey.json')
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://jongproject-c30b3-default-rtdb.firebaseio.com/'
+    'databaseURL': 'https://jongproject-c30b3-default-rtdb.firebaseio.com/',
+    'storageBucket': "jongproject-c30b3.appspot.com"
 })
+ref = db.reference('Image')
+bucket = storage.bucket()
 
 
 def resize(path, imgSize):
     img = Image.open(path)
-
     img_resize = img.resize((imgSize, imgSize), Image.LANCZOS)
     img_resize.save(path)
 
 
+def getCountFromDB():
+    return len(ref.get())
+
+
+def getImgFromDB():
+    # snapshot = ref.order_by_key().get()
+    # first_item = snapshot.popitem(last=True)
+    # img_info = first_item[1]
+    # img_data = img_info.get('image')
+    # img_size_x = img_info.get('size_x')
+    # img_size_y = img_info.get('size_y')
+
+    # image = Image.frombytes('RGBA', (img_size_x, img_size_y), img_data, 'raw')
+    # image.show()
+    blob = bucket.blob()
+    blob.download_to_filename('car')
+
+
+def UploadImage(file):
+    blob = bucket.blob(file)
+    # upload file
+    blob.upload_from_filename(filename=file, content_type='image/jpeg')
+    # print(blob.public_url)
+
+    ref = db.reference('Image/202111241211')
+    ref.set({'url': blob.public_url,
+            'size_x': 3840, 'size_y': 4160})
+
+
 if __name__ == '__main__':
     print("Hello World")
-    #resize('car/white_car.jpg', 416)
-    ref = db.reference()
-    ref.push({'company': 'google'})
+    # resize('car/white_car.jpg', 416)
+
+    path = 'car/red_Car.jpg'
+
+    UploadImage(path)
+
+    # getImgFromDB()
+    # getCountFromDB()
