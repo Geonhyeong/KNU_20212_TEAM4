754cfd4e9f269befbfc3e2861812c1f250499449
fbwlsgh93 <42794341+fbwlsgh93@users.noreply.github.com>
2021-11-25 23:51:03 +0900

    gps 기능 추가

--- send_img_app/app/src/main/java/com/example/disabledparking/MainActivity.kt
+++ send_img_app/app/src/main/java/com/example/disabledparking/MainActivity.kt
@@ -2,9 +2,14 @@ package com.example.disabledparking
 
 import android.app.Activity
 import android.app.AlertDialog
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.ImageDecoder
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationManager
 import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -12,10 +17,12 @@ import android.os.Bundle
 import android.os.Environment
 import android.os.Handler
 import android.provider.MediaStore
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import androidx.core.view.isVisible
@@ -28,6 +35,7 @@ import java.io.File
 import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
+import android.Manifest
 
 class MainActivity : AppCompatActivity() {
     private var selectedUri: Uri? = null
@@ -38,7 +46,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var imageUploadButton: Button
     private lateinit var imageView: ImageView
     private lateinit var fileName: String
-    lateinit var currentPhotoPath : String
+    private lateinit var location: String
+    lateinit var currentPhotoPath: String
+    private var locationManager: LocationManager? = null
+    private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
+    private val PERMISSIONS_REQUEST_CODE = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,14 +62,14 @@ class MainActivity : AppCompatActivity() {
         sendButton = findViewById(R.id.sendButton)
         imageUploadButton = findViewById(R.id.imageUploadButton)
         imageView = findViewById(R.id.imageView)
-        val location = ""
+        location = ""
 
         takePictureButton.setOnClickListener {
             when {
                 (ContextCompat.checkSelfPermission(
                     this,
                     android.Manifest.permission.WRITE_EXTERNAL_STORAGE
-                ) == PackageManager.PERMISSION_GRANTED)  && (ContextCompat.checkSelfPermission(
+                ) == PackageManager.PERMISSION_GRANTED) && (ContextCompat.checkSelfPermission(
                     this,
                     android.Manifest.permission.CAMERA
                 ) == PackageManager.PERMISSION_GRANTED) -> {
@@ -71,7 +83,6 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-
 
         imageUploadButton.setOnClickListener {
             when {
@@ -97,6 +108,7 @@ class MainActivity : AppCompatActivity() {
                 val photoUri = selectedUri ?: return@setOnClickListener
                 uploadPhoto(photoUri,
                     successHandler = { uri ->
+                        getLocation()
                         uploadImage(uri, location)
                     },
                     errorHandler = {
@@ -128,6 +140,38 @@ class MainActivity : AppCompatActivity() {
                     Toast.makeText(this, "권한을 거부하셨습니다.", Toast.LENGTH_SHORT).show()
                 }
         }
+
+        if (requestCode == PERMISSIONS_REQUEST_CODE && grantResults.size == REQUIRED_PERMISSIONS.size) {
+            var check_result = true
+            for (result in grantResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    check_result = false;
+                    break;
+                }
+            }
+            if (check_result) {
+            } else {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        this,
+                        REQUIRED_PERMISSIONS[0]
+                    )
+                    || ActivityCompat.shouldShowRequestPermissionRationale(
+                        this,
+                        REQUIRED_PERMISSIONS[1]
+                    )
+                ) {
+                    Toast.makeText(
+                        this,
+                        "권한 설정이 거부되었습니다.\n앱을 사용하시려면 다시 실행해주세요.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    finish()
+                } else {
+                    Toast.makeText(this, "권한 설정이 거부되었습니다.\n설정에서 권한을 허용해야 합니다..", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -154,10 +198,11 @@ class MainActivity : AppCompatActivity() {
                     val bitmap = MediaStore.Images.Media
                         .getBitmap(contentResolver, Uri.fromFile(file))
                     imageView.setImageBitmap(bitmap)
-                }
-                else{
-                    val decode = ImageDecoder.createSource(this.contentResolver,
-                        Uri.fromFile(file))
+                } else {
+                    val decode = ImageDecoder.createSource(
+                        this.contentResolver,
+                        Uri.fromFile(file)
+                    )
                     val bitmap = ImageDecoder.decodeBitmap(decode)
                     imageView.setImageBitmap(bitmap)
                 }
@@ -178,13 +223,13 @@ class MainActivity : AppCompatActivity() {
     private fun startCameraScreen() {
         Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
             takePictureIntent.resolveActivity(packageManager)?.also {
-                val photoFile: File? = try{
+                val photoFile: File? = try {
                     createImageFile()
-                }catch(ex: IOException){
+                } catch (ex: IOException) {
                     null
                 }
-                photoFile?.also{
-                    val photoURI : Uri = FileProvider.getUriForFile(
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
                         this,
                         "com.example.disabledparking.fileprovider",
                         it
@@ -197,13 +242,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun createImageFile(): File {
-        val timeStamp : String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        val storageDir : File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
         return File.createTempFile(
             "JPEG_${timeStamp}_",
             ".jpg",
             storageDir
-        ).apply{
+        ).apply {
             currentPhotoPath = absolutePath
         }
     }
@@ -253,4 +298,47 @@ class MainActivity : AppCompatActivity() {
     private fun hideProgress() {
         findViewById<ProgressBar>(R.id.progressBar).isVisible = false
     }
+
+    private fun getLocation() {
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager?
+        var userLocation: Location = getLatLng()
+        if (userLocation != null) {
+            var latitude = userLocation.latitude
+            var longitude = userLocation.longitude
+            var mGeoCoder = Geocoder(applicationContext, Locale.KOREAN)
+            var mResultList: List<Address>? = null
+            try {
+                mResultList = mGeoCoder.getFromLocation(latitude!!, longitude!!, 1)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+            if (mResultList != null) {
+                location = mResultList[0].getAddressLine(0)
+            }
+        }
+    }
+
+    private fun getLatLng(): Location{
+        var currentLatLng: Location? = null
+        var hasFineLocationPermission = ContextCompat.checkSelfPermission(this,
+            Manifest.permission.ACCESS_FINE_LOCATION)
+        var hasCoarseLocationPermission = ContextCompat.checkSelfPermission(this,
+            Manifest.permission.ACCESS_COARSE_LOCATION)
+
+        if(hasFineLocationPermission == PackageManager.PERMISSION_GRANTED &&
+            hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED){
+            val locatioNProvider = LocationManager.GPS_PROVIDER
+            currentLatLng = locationManager?.getLastKnownLocation(locatioNProvider)
+        }else{
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[0])){
+                Toast.makeText(this, "앱을 실행하려면 위치 접근 권한이 필요합니다.", Toast.LENGTH_SHORT).show()
+                ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, PERMISSIONS_REQUEST_CODE)
+            }else{
+                ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, PERMISSIONS_REQUEST_CODE)
+            }
+            currentLatLng = getLatLng()
+        }
+        return currentLatLng!!
+    }
+
 }
